2a0333cc0ee67978878b8625b52f62d791a86075
zchmielewska <zchmielewska@gmail.com>
2023-02-27 09:08:28 +0100

    start chapter 8

---
+++ lafore/chapter_8/example/postfix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Counter {
+    private:
+        unsigned int count;
+    public:
+        Counter() : count(0) {}
+        Counter(int c) : count(c) {}
+
+        unsigned int get_count() const {
+            return count;
+        }
+
+        Counter operator ++ () {
+            return Counter(++count);
+        }
+
+        Counter operator ++ (int) {
+            return Counter(count++);
+        }
+};
+
+int main() {
+    Counter c1, c2;
+
+    cout << "\nc1=" << c1.get_count();
+    cout << "\nc2=" << c2.get_count();
+
+    ++c1;
+    c2 = ++c1;
+
+    cout << "\nc1=" << c1.get_count();
+    cout << "\nc2=" << c2.get_count();
+    return 0;
+}
