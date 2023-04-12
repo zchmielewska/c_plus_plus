1ed1a6639365012d0e3718afe8ccc4de418141f4
zchmielewska <zchmielewska@gmail.com>
2023-04-12 19:05:26 +0200

    solve ch4ex12

---
+++ lafore/chapter_4/ex12.cpp
@@ -0,0 +1,54 @@
+/*Revise the four-function fraction calculator program of Exercise 12 in Chapter 3 so that
+each fraction is stored internally as a variable of type struct fraction, as discussed in
+Exercise 8 in this chapter.*/
+
+#include <iostream>
+
+using namespace std;
+
+struct fraction {
+    int num;
+    int den;
+};
+
+
+int main() {
+    fraction f1, f2, f3;
+    char op, dummy, decision;
+
+    while (decision != 'n') {
+        cout << "Enter the first fraction, an operator, and a second fraction: ";
+        cin >> f1.num >> dummy >> f1.den >> op >> f2.num >> dummy >> f2.den;
+
+        switch (op) {
+            case '+':
+                f3.num = f1.num*f2.den + f1.den*f2.num;
+                f3.den = f1.den*f2.den;
+                break;
+
+            case '-':
+                f3.num = f1.num*f2.den - f1.den*f2.num;
+                f3.den = f1.den*f2.den;
+                break;
+
+            case '*':
+                f3.num = f1.num*f2.num;
+                f3.den = f1.den*f2.den;
+                break;
+
+            case '/':
+                f3.num = f1.num*f2.den;
+                f3.den = f1.den*f2.num;
+                break;
+
+            default:
+                cout << "Something went wrong.";
+                return 0;
+        }
+        cout << "Result = " << f3.num << "/" << f3.den << endl; 
+        cout << "Do you want to continue (y/n)? ";
+        cin >> decision;
+    }
+
+    return 0;
+}
